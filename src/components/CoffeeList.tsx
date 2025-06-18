647edd0740f44235e98750e6ecb7546a49d73344
Haroon Saeed <haroon.saeed@outlook.com>
2025-06-19 01:20:54 +0200

    imrove deployment fixes

--- src/components/CoffeeList.tsx
+++ src/components/CoffeeList.tsx
@@ -5,23 +5,19 @@ const CoffeeList = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch(
-      "https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/4-frontend-libaries/challenges/group_1/data/simple-coffee-listing-data.json"
-    )
+    fetch("/data.json")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        setCoffeeList(data);
-
+        setCoffeeList(data.coffeeList);
         setIsLoading(false);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
-  console.log(coffeeList[0], "coffeeList");
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div className="text-center text-white">Loading...</div>;
   }
 
   return (
